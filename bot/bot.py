b26d77be93e0c5afbf4a8f6635636699b0bb23da
baxromov <baxromov.shahzodbek@gmail.com>
2022-06-23 20:45:49 +0500

    add pytelegrambotapi

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,18 @@
 import telebot
-
+import logging
 from config import BOT_TOKEN
-
+logging.basicConfig(level=logging.INFO)
 TOKEN = BOT_TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    print(message)
+    logging.info(message)
+    pass
+
+
+def main():
+    bot.polling()
