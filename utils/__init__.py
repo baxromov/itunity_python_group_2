bbc3df38a7d79865df31f1fbe4eea31794e58896
baxromov <baxromov.shahzodbek@gmail.com>
2022-06-25 21:24:31 +0500

    add

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+# def validate(date_text):
+#     try:
+#         datetime.datetime.strptime(date_text, '%Y-%m-%d')
+#     except ValueError:
+#         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
--- utils/utils.py
+++ utils/utils.py
@@ -1,18 +1,53 @@
 from typing import Optional
+import re
+import datetime
 
 
-def check_fullname(full_name: Optional[str]) -> Optional[bool]:
-    pass
+class Validations:
+    """
+        Validations
+    """
+
+    @staticmethod
+    def validate_email(email: Optional[str]) -> Optional[bool]:
+        """
+            Validate email
+        :param email: string
+        :return: boolean
+        """
+        rule = r"^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$"
+        return True if re.search(rule, email) else False
+
+    @staticmethod
+    def validate_phone_number(phone_number: Optional[str]) -> Optional[bool]:
+        """
+            Validate phone number
+        :param phone_number: string
+        :return: boolean
+        """
+        rule = r"^\+(998)[0-9]{9}$"
+        return True if re.search(rule, phone_number) else False
+
+    @staticmethod
+    def validate_date(date: Optional[str]) -> Optional[bool]:
+        """
+            Validate date
+        :param date: string
+        :return: boolean
+        """
+        try:
+            datetime.datetime.strptime(date, "%d.%m.%Y")
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
 
-def check_phone_number(phone_number: Optional[str]) -> Optional[bool]:
-    pass
-
-
-def check_date(birth_date: Optional[str]) -> Optional[bool]:
-    pass
-
-
-def check_email(email: Optional[str]) -> Optional[bool]:
-    pass
-
+if __name__ == "__main__":
+    # Validate date
+    validator = Validations()
+    print("Mail Validation\n", validator.validate_email("baxromov_shahzodbek@gmail.com"), "\n--------------------")
+    phone = "+998901234567"
+    print("Phone Validation\n", validator.validate_phone_number(phone), "\n--------------------")
+    print(len(phone))
+    print("Date Validation\n", validator.validate_date("20.02.1000"), "\n--------------------")
